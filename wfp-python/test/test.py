1c68ddfcd3727b6b78e4887fd58a7d26d6f82064
huyang09 <huyang09@baidu.com>
2017-02-18 13:55:05 +0800

    hi

--- wfp-python/test/test.py
+++ wfp-python/test/test.py
@@ -1,13 +1,21 @@
 __author__ = 'huyang'
 
-import os
-import platform
-def getSeparator():
-    if 'Windows' in platform.system():
-        separator = '\\'
-    else:
-        separator = '/'
-    return separator
+from pymongo import MongoClient
+from pymongo import ASCENDING
+import re
 
-print(getSeparator())
-print(os.getcwd())
+client = MongoClient('192.168.1.110', 27017)
+
+db = client.get_database('wfp_spider')
+collection_names = db.collection_names()
+sum = 0
+for name in collection_names:
+    if re.match('com_page_\d{6}', name):
+        stock_code = name[9:15]
+        # db.get_collection(name).create_index([('stockCode', ASCENDING), ('url', ASCENDING)], unique=True)
+        cnt = db.get_collection(name).count({'stockCode': stock_code})
+        print(stock_code, cnt)
+
+        sum += cnt
+
+print('total', sum)
