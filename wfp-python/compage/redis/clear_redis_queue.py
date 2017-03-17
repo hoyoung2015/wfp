db428441c014ea1f8f1a5d1745e6c81985bbfb93
huyang09 <huyang09@baidu.com>
2017-03-18 03:11:24 +0800

    hi

--- wfp-python/compage/redis/clear_redis_queue.py
+++ wfp-python/compage/redis/clear_redis_queue.py
@@ -19,7 +19,7 @@ from common import url_utils
 rds = Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
 
 url = 'http://www.haoxiangni.cn/'
-regex = 'http://www.haoxiangni.cn/.+(pageid=\d+){2,}'
+regex = 'http://www.haoxiangni.cn/.+(&pageid=\d+){2,}'
 domain = url_utils.get_domain(url)
 # exit(-1)
 
--- wfp-python/compage/redis/redis_queue_size.py
+++ wfp-python/compage/redis/redis_queue_size.py
@@ -1,8 +1,5 @@
-import os
-import sys
 from redis import Redis
 
-sys.path.append(rootPath)
 
 
 host = '127.0.0.1'
